7844a3f7c9654615aa6f20907b16fbe2b2fc52b4
Francisco Jose Benitez Lazaro <paco.benitez.lazaro@gmail.com>
2023-11-27 19:23:40 +0100

    Refactor test

--- PasswordValidator.Tests/UnitTest1.cs
+++ PasswordValidator.Tests/UnitTest1.cs
@@ -3,17 +3,33 @@ namespace PasswordValidator.Tests;
 [TestClass]
 public class PasswordValidatorShould
 {
+
+    const string VALID_PASSWORD = "123aA45678";
+
     [TestMethod]
     public void ValidateWhenValidaPassword()
     {
-        Assert.AreEqual(true, PasswordValidator.validate("123456789"));
+        Assert.AreEqual(true, PasswordValidator.validate(VALID_PASSWORD));
     }
 
     [TestMethod]
     public void NotValidateWhenHasMoreThanEightCharacters()
     {
-        Assert.AreEqual(false, PasswordValidator.validate("1234567"));
+        Assert.AreEqual(false, PasswordValidator.validate(VALID_PASSWORD.Substring(0, 7)));
     }
 
-   
+    [TestMethod]
+    public void NotValidateThereAreNoCapitalLetters()
+    {
+        Assert.AreEqual(false, PasswordValidator.validate(VALID_PASSWORD.ToLower()));
+    }
+
+    [TestMethod]
+    public void NotValidateThereAreNoLowerLetters()
+    {
+        Assert.AreEqual(false, PasswordValidator.validate(VALID_PASSWORD.ToUpper()));
+    }
+
+
+
 }
--- PasswordValidator/Library.fs
+++ PasswordValidator/Library.fs
@@ -1,5 +1,13 @@
 ﻿namespace PasswordValidator
 
+open System
+
 module PasswordValidator =
+    let containsCapitalLetters (password : string): bool =
+        password |> String.exists Char.IsUpper
+
+    let containsLowerLetters (password : string): bool =
+        password |> String.exists Char.IsLower
+
     let validate (password: string): bool =
-        password.Length > 8
+        password.Length > 8 && containsCapitalLetters password && containsLowerLetters password
