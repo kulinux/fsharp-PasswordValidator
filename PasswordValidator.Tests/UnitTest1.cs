011509a0d32cf447434fb5f8cdfcf98e94bf0b26
Francisco Jose Benitez Lazaro <paco.benitez.lazaro@gmail.com>
2023-12-01 19:51:36 +0100

    Something i can compose

--- PasswordValidator.Tests/UnitTest1.cs
+++ PasswordValidator.Tests/UnitTest1.cs
@@ -1,39 +1,43 @@
+using Microsoft.FSharp.Core;
+
 namespace PasswordValidator.Tests;
 
 [TestClass]
 public class PasswordValidatorShould
 {
 
+    FSharpFunc<string, bool> passwordValidator = PasswordValidator.validation(ValidationType.ValidationOne);
+
     const string VALID_PASSWORD = "123aA_5678";
 
     [TestMethod]
     public void ValidateWhenValidaPassword()
     {
-        Assert.AreEqual(true, PasswordValidator.validate(VALID_PASSWORD));
+        Assert.AreEqual(true, passwordValidator.Invoke(VALID_PASSWORD));
     }
 
     [TestMethod]
     public void NotValidateWhenHasMoreThanEightCharacters()
     {
-        Assert.AreEqual(false, PasswordValidator.validate(VALID_PASSWORD.Substring(0, 7)));
+        Assert.AreEqual(false, passwordValidator.Invoke(VALID_PASSWORD.Substring(0, 7)));
     }
 
     [TestMethod]
     public void NotValidateThereAreNoCapitalLetters()
     {
-        Assert.AreEqual(false, PasswordValidator.validate(VALID_PASSWORD.ToLower()));
+        Assert.AreEqual(false, passwordValidator.Invoke(VALID_PASSWORD.ToLower()));
     }
 
     [TestMethod]
     public void NotValidateThereAreNoLowerLetters()
     {
-        Assert.AreEqual(false, PasswordValidator.validate(VALID_PASSWORD.ToUpper()));
+        Assert.AreEqual(false, passwordValidator.Invoke(VALID_PASSWORD.ToUpper()));
     }
 
     [TestMethod]
     public void NotValidateThereAreNoUnderscore()
     {
-        Assert.AreEqual(false, PasswordValidator.validate(VALID_PASSWORD.Replace("_", "a")));
+        Assert.AreEqual(false, passwordValidator.Invoke(VALID_PASSWORD.Replace("_", "a")));
     }
 
 
--- PasswordValidator/Library.fs
+++ PasswordValidator/Library.fs
@@ -2,16 +2,21 @@
 
 open System
 
-module PasswordValidator =
+type ValidationType =
+    | ValidationOne
+    | ValidationTwo
 
-    let containsCapitalLetters (password: string) : bool = password |> String.exists Char.IsUpper
 
-    let containsLowerLetters (password: string) : bool = password |> String.exists Char.IsLower
+module private PasswordValidatorTypeOne =
 
-    let containsUnderscore (password: string) : bool =
+    let private containsCapitalLetters (password: string) : bool = password |> String.exists Char.IsUpper
+
+    let private containsLowerLetters (password: string) : bool = password |> String.exists Char.IsLower
+
+    let private containsUnderscore (password: string) : bool =
         password |> String.exists (fun (c: char) -> c = '_')
 
-    let validLength (password: string) : bool = password.Length > 8
+    let private validLength (password: string) : bool = password.Length > 8
 
     let validate (password: string) : bool =
         let validations =
@@ -23,3 +28,7 @@ module PasswordValidator =
         let valids = validations |> List.filter (fun pred -> pred password)
 
         valids.Length = validations.Length
+
+
+module PasswordValidator =
+    let validation (validationType: ValidationType): string -> bool = PasswordValidatorTypeOne.validate
