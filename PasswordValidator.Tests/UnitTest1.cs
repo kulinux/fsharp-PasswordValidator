aff58713d7c966792aa28c624491028d9083ce0b
Francisco Jose Benitez Lazaro <paco.benitez.lazaro@gmail.com>
2023-12-02 06:34:20 +0100

    Some refactor

--- PasswordValidator.Tests/UnitTest1.cs
+++ PasswordValidator.Tests/UnitTest1.cs
@@ -3,41 +3,40 @@ using Microsoft.FSharp.Core;
 namespace PasswordValidator.Tests;
 
 [TestClass]
-public class PasswordValidatorShould
+public class PasswordValidatorOneShould
 {
-
-    FSharpFunc<string, bool> passwordValidator = PasswordValidator.validation(ValidationType.ValidationOne);
-
     const string VALID_PASSWORD = "123aA_5678";
+
+    private static Func<string, bool> validate = PasswordValidator.validation(ValidationType.ValidationOne).Invoke;
 
     [TestMethod]
     public void ValidateWhenValidaPassword()
     {
-        Assert.AreEqual(true, passwordValidator.Invoke(VALID_PASSWORD));
+        Assert.AreEqual(true, validate(VALID_PASSWORD));
     }
 
     [TestMethod]
     public void NotValidateWhenHasMoreThanEightCharacters()
     {
-        Assert.AreEqual(false, passwordValidator.Invoke(VALID_PASSWORD.Substring(0, 7)));
+        Assert.AreEqual(false, validate(VALID_PASSWORD.Substring(0, 7)));
     }
 
     [TestMethod]
     public void NotValidateThereAreNoCapitalLetters()
     {
-        Assert.AreEqual(false, passwordValidator.Invoke(VALID_PASSWORD.ToLower()));
+        Assert.AreEqual(false, validate.Invoke(VALID_PASSWORD.ToLower()));
     }
 
     [TestMethod]
     public void NotValidateThereAreNoLowerLetters()
     {
-        Assert.AreEqual(false, passwordValidator.Invoke(VALID_PASSWORD.ToUpper()));
+        Assert.AreEqual(false, validate(VALID_PASSWORD.ToUpper()));
     }
 
     [TestMethod]
     public void NotValidateThereAreNoUnderscore()
     {
-        Assert.AreEqual(false, passwordValidator.Invoke(VALID_PASSWORD.Replace("_", "a")));
+        Assert.AreEqual(false, validate(VALID_PASSWORD.Replace("_", "a")));
     }
 
 
